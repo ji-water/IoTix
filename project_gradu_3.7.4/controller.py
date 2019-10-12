9ac5b1d3a91096b5e579581beed4193f545cbb4d
ji-water <bjisoo72@gmail.com>
2019-10-12 14:21:35 +0900

    [HTML]
    [ ji-water, 191012 ] charts.html 완료, url 전달방식 변경

--- project_gradu_3.7.4/controller.py
+++ project_gradu_3.7.4/controller.py
@@ -91,6 +91,7 @@ class HomeAPI(Resource):
             return make_response(render_template('index.html'))
 
 class FarmAPI(Resource):
+
     @login_required
     def get(self):
         if not session.get('logged_in'):
@@ -106,34 +107,61 @@ class FarmAPI(Resource):
                 'manager':farm_data.manager.user_name,
                 'position_data' : position_data
             }
+
+            p_list = [0] * 9
+            for x in data['position_data']:
+                p_list[x['position_num'] - 1] = 1
+            data['position_list'] = p_list
+
             #return make_response(json.dumps(data,ensure_ascii=False),200)
-            return make_response(render_template('farm.html',id=data['id'],name=data['name'],manager=data['manager'],position_data=data['position_data']))
+            return make_response(render_template('farm.html',id=data['id'],name=data['name'],manager=data['manager'],position_data=data['position_data'],p_list=data['position_list']))
+
+    @login_required
+    def post(self):
+        position_num = request.form['position_num']
+        return redirect(url_for('cropdetailapi',position_num=position_num))
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('crop', type=str,default=None)
 parser.add_argument('crop_part',type=str,default=None)
 parser.add_argument('date',type=str, default=None)
 
+class CropAPI(Resource):
+    @login_required
+    def get(self,position_num,crop_name):
+        farm_data = Farm.get_farm_by_user(current_user.get_id())
+        crop_data = Crop.get_crop_by_name(farm_data.pk, position_num,crop_name)
+        part_name_data = CropPart.get_having_part_name(crop_data['_id'])
+
+        return part_name_data
+
 class CropDetailAPI(Resource):
     @login_required
     def get(self,position_num):
-        crop_data = Crop.get_crop_by_position('5d77d915acf3296b9e3c1c73',position_num)
+        farm_data = Farm.get_farm_by_user(current_user.get_id())  # farm_id받아올수있으면 필요없는 과정
+        crop_data = Crop.get_crop_by_position(farm_data.pk, position_num)
 
         #crop에 해당하는 part 정보도 포함
         for data in crop_data:
             part_name_data = CropPart.get_having_part_name(data['_id'])
             data['crop_part']=part_name_data #중복없이 crop_part_name list만 추가
 
-        return crop_data
+        chart_data = [{'delta':0},{'delta':0},{'delta':0},{'delta':0},{'delta':0},{'delta':0},{'delta':0}]
+        date_select=0
+
+        #return crop_data
+        return make_response(render_template('charts.html',position_num=position_num,data=crop_data,chart_list=chart_data,date_select=date_select))
 
     @login_required
     def post(self,position_num):
         #구현중
+        farm_data = Farm.get_farm_by_user(current_user.get_id())  # farm_id받아올수있으면 필요없는 과정
         crop_name = request.form['crop_name']
         part_name = request.form['crop_part_name']
-        if request.form['date'] :
+        if request.form['date'] != 00000000:
             date_temp = request.form['date'] #format : 20191005
-        else :
+        else:
             date_temp = datetime.datetime.now().strftime("%Y%m%d")
 
         date_obj = datetime.datetime.strptime(date_temp, "%Y%m%d")
@@ -147,8 +175,8 @@ class CropDetailAPI(Resource):
         list_for_graph = list() #7일의 delta만 담음
 
         for i in range(gap+1) :
-            temp_qs = CropPart.get_crop_part_day_detail(crop_name,part_name,date_obj+timedelta(days=i))
-            yes_qs  = CropPart.get_crop_part_day_detail(crop_name,part_name,date_obj+timedelta(days=i-1))
+            temp_qs = CropPart.get_crop_part_day_detail(farm_data.pk, crop_name, part_name, date_obj + timedelta(days=i))
+            yes_qs = CropPart.get_crop_part_day_detail(farm_data.pk, crop_name, part_name, date_obj + timedelta(days=i - 1))
             temp_qs['delta']= temp_qs['length']-yes_qs['length'] #변화량 추가
 
             list_for_graph.append(temp_qs['delta'])
@@ -162,8 +190,11 @@ class CropDetailAPI(Resource):
             result_list.append(tp_dict)
 
         print(list_for_graph)
+        crop_data = Crop.get_crop_by_position(farm_data.pk, position_num)
+        date_select = 1
 
-        return result_list
+        #return result_list
+        return make_response(render_template('charts.html', position_num=position_num, data=crop_data, chart_list=result_list, date_select=date_select))
 
 api.add_resource(HomeAPI, '/')
 api.add_resource(LoginAPI, '/login')
@@ -171,8 +202,10 @@ api.add_resource(LogoutAPI, '/logout')
 #api.add_resource(FarmAPI,'/<ObjectId:user_pk>')
 #api.add_resource(FarmAPI,'/farm/<ObjectId:farm_id>')
 api.add_resource(FarmAPI, '/farm')
+api.add_resource(CropAPI, '/farm/<int:position_num>/<string:crop_name>')
 #api.add_resource(CropDetailAPI,'/farm/crop_detail')
 api.add_resource(CropDetailAPI, '/farm/<int:position_num>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- project_gradu_3.7.4/model.py
+++ project_gradu_3.7.4/model.py
@@ -72,6 +72,22 @@ class Crop():
         return list(map(lambda crop: crop.to_mongo(), qs))
 
     @staticmethod
+    def get_crop_by_name(farm_id, position_num, crop_name):
+        position_qs = PositionSchema.objects(farm=farm_id, position_num=position_num).first()
+        qs = CropSchema.objects(farm=farm_id, position=position_qs.id, crop_name=crop_name)
+        if qs.count == 0:
+            return None
+        qs_list = list(map(lambda crop: crop.to_mongo(), qs))  # convert to list
+
+        # parse objectid to str
+        for data in qs_list:
+            for key, value in data.items():
+                if isinstance(value, ObjectId):
+                    data[key] = str(value)
+
+        return qs_list[0]
+
+    @staticmethod
     def get_crop_by_position(farm_id, position_num):
         position_qs = PositionSchema.objects(farm=farm_id,position_num=position_num).first()
         qs = CropSchema.objects(farm=farm_id,position=position_qs.id)
@@ -124,14 +140,15 @@ class CropPart():
         return qs_list
 
     @staticmethod
-    def get_crop_part_detail(crop_name, part_name, date_obj):
+    def get_crop_part_detail(farm_id, crop_name, part_name, date_obj):
         #date_obj = datetime.strptime(date, "%Y%m%d")
         date_obj2 = date_obj+timedelta(days=1)
         print(date_obj2)
 
-        crop_qs = CropSchema.objects(crop_name=crop_name,farm='5d77d915acf3296b9e3c1c73').first()
+        crop_qs = CropSchema.objects(crop_name=crop_name,farm=farm_id).first()
         part_qs = CropPartSchema.objects.filter(crop=crop_qs.pk,crop_part_name=part_name)
         date_qs = part_qs.filter(__raw__={"date": {"$gte": date_obj, "$lt": date_obj2}})
+
         #date 쿼리 추가
         if part_qs.count == 0:
             return None
@@ -149,15 +166,23 @@ class CropPart():
         return qs_list
 
     @staticmethod
-    def get_crop_part_day_detail(crop_name, part_name, date_obj):
+    def get_crop_part_day_detail(farm_id,crop_name, part_name, date_obj):
+
+        print(farm_id,crop_name,part_name,date_obj)
+
         #date_obj = datetime.strptime(date, "%Y%m%d")
         date_obj2 = date_obj+timedelta(days=1)
+        print(date_obj2)
 
-        crop_qs = CropSchema.objects(crop_name=crop_name,farm='5d77d915acf3296b9e3c1c73').first()
+        crop_qs = CropSchema.objects(crop_name=crop_name,farm=farm_id).first()
+        print(crop_qs)
         part_qs = CropPartSchema.objects.filter(crop=crop_qs.pk,crop_part_name=part_name)
+        print(part_qs)
         date_qs = part_qs.filter(__raw__={"date": {"$gte": date_obj, "$lt": date_obj2}})
+        print(date_qs)
 
-        if date_qs.count==0:
+        if date_qs.count()==0:
+            print("---None---")
             return None
         data = date_qs.order_by('-id').first().to_mongo()
 
@@ -211,31 +236,33 @@ class Position():
 if __name__ == '__main__':
     ##지수 user id '5d943df97689541378a2540f'
     ##지수 farm id '5d95c39e0d93ac9e7b820ce2'
-    ##지수 position id '5d95c3f7cd8c9473a8840692'
-    ##지수 crop id '5d95c445451de46928d16d85'
+    ##지수 position id _1 '5d998f78e179752adb6b880f'
+    ##      position id 2 '5d998f86d07e9dbd486b78bd'
+    ##지수 crop id tomato1 '5d999068dbc2308bb0794aab'
+    ##              tomato2 '5d99907a612afe8a0bf55b2e'
 
 
     #user_dic = {'user_id': "kkkdeon", 'user_pw': "1234", 'user_name': "김도연"}
     #User.create(user_dic)
 
-    farm_dic = {'name': 'farm1', 'manager': User.get_by_id('5d77d4d4694d62037a1684e7'),'phone_num': '01046244619'}
-    crop_dic = {'farm': Farm.get_by_id('5d77d915acf3296b9e3c1c73'), 'crop_name':"tomato2", 'crop_type':"tomato", 'position': Position.get_by_id('5d9065696b602e2e595d873e')}
-    position_dic = {'farm': Farm.get_by_id('5d77d915acf3296b9e3c1c73'), 'position_num': 2, 'position_abs': "gps주소", 'position_name': "2번 위치 desc"}
+    #farm_dic = {'name': 'farm1', 'manager': User.get_by_id('5d77d4d4694d62037a1684e7'),'phone_num': '01046244619'}
+    #crop_dic = {'farm': Farm.get_by_id('5d95c39e0d93ac9e7b820ce2'), 'crop_name':"tomato2", 'crop_type':"tomato", 'position': Position.get_by_id('5d998f78e179752adb6b880f')}
+    #position_dic = {'farm': Farm.get_by_id('5d95c39e0d93ac9e7b820ce2'), 'position_num': 2, 'position_abs': "gps주소", 'position_name': "2번 위치 desc"}
 
-    str1 = "20191004 23:50:10"
+    str1 = "20191009 3:02:02"
     date_obj1 = datetime.strptime(str1, "%Y%m%d %H:%M:%S")
-    crop_part_dic={'crop': Crop.get_by_id('5d9079e8c54b56aab045d9fb'),'crop_part_name':"stem1",'length':6.00,'speed':0.84,'date': date_obj1}
+    crop_part_dic={'crop': Crop.get_by_id('5d999068dbc2308bb0794aab'),'crop_part_name':"stem1",'length':7.1,'speed':0.6,'date': date_obj1}
     #CropPart.create(crop_part_dic)
 
 
     #farm_dic = {'name': 'farm1', 'manager': User.get_by_id('5d943df97689541378a2540f'),'phone_num': '01046244619'}
     # crop_dic = {'farm': Farm.get_by_id('5d95c39e0d93ac9e7b820ce2'), 'crop_name':"tomato2", 'crop_type':"tomato", 'position': Position.get_by_id('5d95c3f7cd8c9473a8840692')}
     #position_dic = {'farm': Farm.get_by_id('5d95c39e0d93ac9e7b820ce2'), 'position_num': 2, 'position_abs': "gps주소", 'position_name': "2번 위치 desc"}
-    crop_part_dic={'crop': Crop.get_by_id('5d95c445451de46928d16d85'),'crop_part_name':"stem2",'length':3.88,'speed':0.90,'date': datetime.now()}
+    #crop_part_dic={'crop': Crop.get_by_id('5d95c445451de46928d16d85'),'crop_part_name':"stem2",'length':3.88,'speed':0.90,'date': datetime.now()}
 
     #Farm.create(farm_dic)
     #Position.create(position_dic)
-    # Crop.create(crop_dic)
+    #Crop.create(crop_dic)
     #print(Farm.get_farm_by_user('5d77d4d4694d62037a1684e7').to_mongo())
     #print(User.get_by_id('5d77d4d4694d62037a1684e7').pk)
 
