d3109eeb5eace03098cb93ca3437db7521e11f07
kkkdeon <48198099+kkkdeon@users.noreply.github.com>
2019-11-07 19:28:52 +0900

    [_]
    _

--- project_gradu_3.7.4/model.py
+++ project_gradu_3.7.4/model.py
@@ -155,6 +155,7 @@ class CropPart():
 
         qs_list = list(map(lambda part_data: part_data.to_mongo(), date_qs))  # convert to list
 
+
         # parse objectid to str
         for data in qs_list:
             for key, value in data.items():
@@ -169,7 +170,7 @@ class CropPart():
     def get_crop_part_day_detail(farm_id,crop_name, part_name, date_obj):
         #date_obj = datetime.strptime(date, "%Y%m%d")
         date_obj2 = date_obj+timedelta(days=1)
-        print(date_obj2)
+        #print(date_obj2)
 
         crop_qs = CropSchema.objects(crop_name=crop_name,farm=farm_id).first()
         part_qs = CropPartSchema.objects.filter(crop=crop_qs.pk,crop_part_name=part_name)
@@ -180,7 +181,7 @@ class CropPart():
             speed_list.append(data['speed'])
 
         date_qs = part_qs.filter(__raw__={"date": {"$gte": date_obj, "$lt": date_obj2}})
-        print(date_qs)
+        #print(date_qs)
 
         if date_qs.count()==0:
             print("---None---")
@@ -262,10 +263,10 @@ if __name__ == '__main__':
     #crop_dic = {'farm': Farm.get_by_id('5d95c39e0d93ac9e7b820ce2'), 'crop_name':"tomato2", 'crop_type':"tomato", 'position': Position.get_by_id('5d998f78e179752adb6b880f')}
     #position_dic = {'farm': Farm.get_by_id('5d95c39e0d93ac9e7b820ce2'), 'position_num': 2, 'position_abs': "gps주소", 'position_name': "2번 위치 desc"}
 
-    str1 = "20191014 3:02:02"
+    str1 = "20191031 3:02:02"
     date_obj1 = datetime.strptime(str1, "%Y%m%d %H:%M:%S")
-    crop_part_dic={'crop': Crop.get_by_id('5d999068dbc2308bb0794aab'),'crop_part_name':"stem1",'length':7.1,'speed':0.6,'date': date_obj1}
-    CropPart.create(crop_part_dic)
+    crop_part_dic={'crop': Crop.get_by_id('5d9079e8c54b56aab045d9fb'),'crop_part_name':"stem1",'length':10.5,'speed':1.9,'date': date_obj1,'tag':0}
+    #CropPart.create(crop_part_dic)
 
 
     #farm_dic = {'name': 'farm1', 'manager': User.get_by_id('5d943df97689541378a2540f'),'phone_num': '01046244619'}
